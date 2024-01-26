03331c9f5db0d9aef3570f2fe3a8014deffb68d0
Forhadul Islam <mdforhadul44@gmail.com>
2024-01-26 21:50:16 +0600

    add all

--- tech_leaner_backend/settings.py
+++ tech_leaner_backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'account',
+    'books',
+    'payments',
+    'utils',
+    'landing',
 ]
 
 MIDDLEWARE = [
