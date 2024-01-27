e68ddafb647c19b5a03baa8515a9c8388c37f727
Forhadul Islam <mdforhadul44@gmail.com>
2024-01-27 22:07:05 +0600

    add all

--- books/admin.py
+++ books/admin.py
@@ -4,3 +4,7 @@ from books.models import *
 # Register your models here.
 admin.site.register(Categories)
 admin.site.register(Book)
+admin.site.register(Free_book_page)
+admin.site.register(Free_books)
+admin.site.register(Chapter)
+admin.site.register(Pages)
--- books/models.py
+++ books/models.py
@@ -15,17 +15,57 @@ class Categories(models.Model):
         return self.title
 
 
+class Free_book_page(models.Model):
+   book_name = models.CharField(max_length=100, blank=True, null=True)
+   book_page_number = models.CharField(max_length=100, blank=True, null=True)
+   book_image = models.ImageField(upload_to='free_pages/', blank=True, null=True)
+   
+   def __str__(self):
+       return f"{self.book_name} - {self.book_page_number}"
+
+
+
+class Free_books(models.Model):
+   book_name = models.CharField(max_length=100, blank=True, null=True)
+   book_images = models.ManyToManyField(Free_book_page)
+   def __str__(self):
+       return f"{self.book_name}"
+
+   
+
 
 class Book(models.Model):
-   book_title = models.CharField(max_length=30, blank=True, null=True)
-   book_title_bn = models.CharField(max_length=30, blank=True, null=True)
+   book_title = models.CharField(max_length=50, blank=True, null=True)
+   book_title_bn = models.CharField(max_length=50, blank=True, null=True)
    book_details = models.TextField(blank=True, null=True)
    book_details_bn = models.TextField(blank=True, null=True)
    book_subject_code = models.CharField(max_length=10, blank=True, null=True)
    book_session =  models.ForeignKey(Session, on_delete=models.CASCADE)
    book_image =  models.ImageField(upload_to='books/', blank=True, null=True)
+   free_book_image = models.ForeignKey(Free_books, on_delete=models.CASCADE)
       
    def __str__(self):
-        return self.book_title
+        return f"{self.book_title} - {self.book_subject_code}"
 
+
+class Chapter(models.Model):
+   book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+   book_name = models.CharField(max_length=50, blank=True, null=True)
+   chapter_name = models.CharField(max_length=100, blank=True, null=True)
+   chapter_number = models.CharField(max_length=20, blank=True, null=True)
+
+   def __str__(self):
+        return f"{self.book_name} - {self.chapter_name} - {self.chapter_number}"
+
+
+class Pages(models.Model):
+   chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+   book_name= models.CharField(max_length=50, blank=True, null=True)
+   chapter_name = models.CharField(max_length=100, blank=True, null=True)
+   pages_number = models.CharField(max_length=20, blank=True, null=True)
+   pages_images = models.ImageField(upload_to='book_pages/', blank=True, null=True)
    
+   def __str__(self):
+        return f"{self.book_name} - {self.chapter_name} - {self.pages_number}"
+
+
