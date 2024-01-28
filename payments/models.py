cfa826a97d94c247cd40453772ba059fee398924
Forhadul Islam <mdforhadul44@gmail.com>
2024-01-28 12:14:03 +0600

    add all

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+HISTORY_TYPE = (
+    ("-debit", "-debit"),
+    ("+credit", "+credit")
+)
+
+class Balance_History(models.Model):
+   history_type = models.CharField(
+        max_length=20, choices=HISTORY_TYPE, default=' ', blank=True, null=True)
+   amount = models.CharField(max_length=50, blank=True, null=True)
+   date = models.DateTimeField(null=True)
+
+
+class Balance(models.Model):
+   main_balance = models.CharField(max_length=50, blank=True, null=True)
+   balance_history = models.ManyToManyField(Balance_History)
+   
