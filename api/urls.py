0e21c5cc6f003a5a6be5f70dd4b3cd0ed655dddb
Forhadul Islam <mdforhadul44@gmail.com>
2024-01-30 00:57:02 +0600

    complited all models

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,27 @@ from accounts.views import *
 urlpatterns = [
     path('auth/login/', login, name='login'),
     path('auth/register/', register, name='register'),
-    path('auth/logout/', knox_views.LogoutView.as_view(), name='logout'), 
+    path('auth/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    # auth/account/ ----user info
+    # auth/balance/ user account balance
+    # chart/book-readed/ ----book readed chart
+    # chart/site-visted/ ----site visited chart
+    # book/ -----all book
+    # book/?book-name=book-name|sub-code=304950|session=2023|tag=hello
+    # book/<book-id>/ -----single book
+    # book/<book-id>/chapter/ -----all chapter
+    # book/<book-id>/chapter/<chapter-id>/ -----chapter datiels 
+    # book/<book-id>/chapter/<chapter-id>/pages/ -----under chapter all pages
+    # book/categories/ ---all categories show
+    # book/?categories=hello ---filtering categories.....show under categoris books
+    # book/access-book/ --only show user access book
+    # book/readed-book/ --only show user access book
+    # subscription/ --show all Subscription
+    # land/hero/ ---show hero all data
+    # land/subscription/ ---show all subscription plan
+    # land/about/ ---about datiles 
+    # land/about/team-member/ ---about team member datiles 
+    # land/ques-ans/ ----question and answer section
+    # land/help-about/ -----our all deatils
+    # land/help-contact/ ----contact with us
 ]
--- landing/models.py
+++ landing/models.py
@@ -19,6 +19,7 @@ class LandingHero(models.Model):
 class LandingSubscription(models.Model):
    title1 = models.CharField(max_length=20, blank=True, null=True)
    description = models.TextField(blank=True, null=True)
+   type = models.CharField(max_length=100, blank=True, null=True)
    offer_box = models.ManyToManyField(Subscription,blank=True)
    
    
@@ -43,12 +44,26 @@ class About(models.Model):
    
 
 
-class QuestionBox(models.Model):
-   question = models.CharField(max_length=150, blank=True, null=True)
-   answer = models.TextField(blank=True, null=True)
-
 
 class FrequentlyAskedQuestions(models.Model):
+   question = models.CharField(max_length=150, blank=True, null=True)
+   answer = models.TextField(blank=True, null=True)
+   
+   
+   
+class HelpAbout(models.Model):
    title = models.CharField(max_length=50, blank=True, null=True)
    description = models.TextField(blank=True, null=True)
-   question_box = models.ManyToManyField(QuestionBox, blank=True)
+   title1 = models.CharField(max_length=50, blank=True, null=True)
+   description1 = models.TextField(blank=True, null=True)
+   location = models.CharField(max_length=50, blank=True, null=True)
+   number = models.CharField(max_length=20, blank=True, null=True)
+   email = models.CharField(max_length=50, blank=True, null=True)
+   
+   
+   
+class HelpContact(models.Model):
+   name =models.CharField(max_length=30, blank=True, null=True)
+   email =models.CharField(max_length=50, blank=True, null=True)
+   number =models.CharField(max_length=20, blank=True, null=True)
+   message =models.TextField(blank=True, null=True)
