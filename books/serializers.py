fe5c9fbfbb2477efce32d01400eff93385d3250f
Forhadul Islam <mdforhadul44@gmail.com>
2024-02-18 01:13:00 +0600

    add all code

--- books/serializers.py
+++ books/serializers.py
@@ -7,5 +7,28 @@ class CategoriesSerializers(serializers.ModelSerializer):
       fields = "__all__"
    
 
+class FreeBookPageSerializers(serializers.ModelSerializer):
+   class Meta:
+      model = FreeBookPage
+      fields = "__all__"
+      
 
 
+class BookSerializers(serializers.ModelSerializer):
+   class Meta:
+      model = Book
+      fields = "__all__"
+
+
+
+class ChapterSerializers(serializers.ModelSerializer):
+   class Meta:
+      model = Chapter
+      fields = "__all__"
+
+
+
+class PagesSerializers(serializers.ModelSerializer):
+   class Meta:
+      model = Pages
+      fields = "__all__"
